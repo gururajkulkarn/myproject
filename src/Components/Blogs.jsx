ded4af5e3afc47a298bdc5f645aa4d20c43738d2
gururajkulkarn <107684801+gururajkulkarn@users.noreply.github.com>
2023-05-11 13:31:42 +0530

    updated file added to modal

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -4,6 +4,9 @@ const Blogs = () => {
   return (
     <div>
       <h1 style={{display:"flex",alignItems:"center",justifyContent:"center",color:"green"}}>Blogs Page ComingSoon...</h1>
+    
+    <h1>updated data added</h1>
+    
     </div>
   )
 }
